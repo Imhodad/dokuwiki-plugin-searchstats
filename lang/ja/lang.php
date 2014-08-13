0d65cac84acdd8305e6e6a287e1e139c5a3cb43d
sawachan <chuno@live.jp>
2014-08-13 13:24:09 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['menu'] = '検索単語統計';
+$lang['th_word'] = '単語';
+$lang['th_count'] = '件数';
+$lang['top10k'] = '上位 10 の単語';
+$lang['nosearchwords'] = '検索単語がありませんでした。';
